2193ce9300050e7034ac3389a71747bd925b25ec
ionut <opreaionut035@gmail.com>
2024-03-31 23:31:38 +0300

    It's working let's goooooo!

--- src/robot/setup.py
+++ src/robot/setup.py
@@ -23,7 +23,8 @@ setup(
         'console_scripts': [
             'motor_controller = robot.motor_controller:main',
             'websocket=robot.websocket:main',
-            'image_publisher=robot.image_publisher:main'
+            'image_publisher=robot.image_publisher:main',
+            'image_subscriber=robot.image_subscriber:main'
         ],
     },
 )
