13619afc6cb00c11e8bb2addd6cf94585a03afa9
ionut <opreaionut035@gmail.com>
2024-04-08 19:35:14 +0300

    updated more things

--- nodes.sh
+++ nodes.sh
@@ -3,6 +3,10 @@
 if [ ! -d  $(pwd)/install ];then
     colcon build
 fi
+source /opt/ros/iron/setup.sh
+source /usr/share/colcon_cd/function/colcon_cd.sh
+export _colcon_cd_root=/opt/ros/iron/
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
 source install/local_setup.bash 
 ros2 launch robot launch.xml
 
